ebc3e0dc83a2057be2f33eba2ec792f860d39e90
merfan100 <ajabrasmie612@gmail.com>
2025-11-24 02:17:14 +0330

    Create server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import threading
+import app  # این همون فایل بات تلگرام تو هست
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def home():
+    return "Bot is running!"
+
+# اجرای ربات تلگرام در یک Thread جدا
+def run_bot():
+    import app  # همین فایل بات تلگرام تو
+    # app.py خودش polling داره، اجرا میشه
+
+threading.Thread(target=run_bot).start()
+
+if __name__ == "__main__":
+    app_web.run(host="0.0.0.0", port=10000)
