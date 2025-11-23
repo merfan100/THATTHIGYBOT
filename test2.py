72f0aaaea4ed7bd7d21acf254d71ad2fa6314db3
merfan100 <ajabrasmie612@gmail.com>
2025-11-24 02:01:07 +0330

    Update test2.py

--- test2.py
+++ test2.py
@@ -321,7 +321,7 @@ await set_bot_commands(app)
 print("✅ ربات در حال اجراست...")  
 await app.run_polling()
 
-if name == 'main':
+if __name__ == '__main__':
 import nest_asyncio
 nest_asyncio.apply()
 asyncio.get_event_loop().run_until_complete(main())
